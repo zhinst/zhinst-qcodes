27c1f86b10337d9aecf5b5d6800eb0338badd0a0
Max Ruckriegel <max.ruckriegel@zhinst.com>
2020-03-25 15:39:58 +0100

    Add sweeper module in MFLI

--- zhinst/qcodes/mfli.py
+++ zhinst/qcodes/mfli.py
@@ -1,13 +1,17 @@
 from .base import ZIBaseInstrument
-import zhinst.toolkit as tk
-from zhinst.toolkit.control.drivers.base import DAQModule
-from zhinst.toolkit.control.nodetree import Parameter
-
 from qcodes.instrument.channel import InstrumentChannel
 import qcodes.utils.validators as vals
 
+import zhinst.toolkit as tk
+from zhinst.toolkit.control.drivers.mfli import DAQModule, SweeperModule
+
 
 class DAQ(InstrumentChannel):
+    """
+    test docstring here
+    
+    """
+
     def __init__(self, name, parent_instr, parent_contr):
         super().__init__(parent_instr, name)
         self._daq_module = DAQModule(parent_contr)
@@ -54,6 +58,64 @@ class DAQ(InstrumentChannel):
         return self._daq_module.results
 
 
+class Sweeper(InstrumentChannel):
+    """
+    sweeper docstring here
+    
+    """
+
+    def __init__(self, name, parent_instr, parent_contr):
+        super().__init__(parent_instr, name)
+        self._sweeper_module = SweeperModule(parent_contr)
+        self._sweeper_module._setup()
+        for k, param in self._sweeper_module.__dict__.items():
+            if not k.startswith("_"):
+                val_mapping = None
+                if param._map:
+                    val_mapping = dict(map(reversed, param._map.items()))
+                    param._map = None
+                self.add_parameter(
+                    k,
+                    get_cmd=param,
+                    set_cmd=param,
+                    docstring=param._description,
+                    unit=param._unit,
+                    val_mapping=val_mapping,
+                )
+
+    def signals_add(self, signal_source):
+        return self._sweeper_module.signals_add(signal_source)
+
+    def signals_clear(self):
+        self._sweeper_module.signals_clear()
+
+    def signals_list(self):
+        return self._sweeper_module.signals_list()
+
+    def sweep_parameter(self, param):
+        self._sweeper_module.sweep_parameter(param)
+
+    def measure(self, **kwargs):
+        self._sweeper_module.measure(**kwargs)
+
+    def application(self, application):
+        self._sweeper_module.application(application)
+
+    def _set(self, *args):
+        self._sweeper_module._set(*args)
+
+    def _get(self, *args, valueonly=True):
+        return self._sweeper_module._get(*args)
+
+    @property
+    def signals(self):
+        return self._sweeper_module.signals
+
+    @property
+    def results(self):
+        return self._sweeper_module.results
+
+
 class MFLI(ZIBaseInstrument):
     """
     QCoDeS driver for ZI MFLI.
@@ -62,9 +124,16 @@ class MFLI(ZIBaseInstrument):
     """
 
     def __init__(
-        self, name: str, serial: str, interface="1gbe", host="localhost", **kwargs
+        self,
+        name: str,
+        serial: str,
+        interface="1gbe",
+        host="localhost",
+        port=8004,
+        api=6,
+        **kwargs
     ) -> None:
-        super().__init__(name, "mfli", serial, interface, host)
+        super().__init__(name, "mfli", serial, interface, host, port, api, **kwargs)
         submodules = self.nodetree_dict.keys()
         blacklist = ["scopes"]
         [self._init_submodule(key) for key in submodules if key not in blacklist]
@@ -79,6 +148,7 @@ class MFLI(ZIBaseInstrument):
         self.connect_message()
         self._get_nodetree_dict()
         self.add_submodule("daq", DAQ("daq", self, self._controller))
+        self.add_submodule("sweeper", Sweeper("sweeper", self, self._controller))
 
     def get_idn(self):
         return dict(
