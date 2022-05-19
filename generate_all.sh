20a7543e9f8c34ab6d5b9e90c29acbbf3784a500
Markus Heikkinen <markus.heikkinen@zhinst.com>
2022-05-19 14:28:27 +0200

    Create separate code generator package

--- generate_all.sh
+++ generate_all.sh
@@ -2,10 +2,10 @@
 # This should move to some github action
 
 # toolkit driver
-python generator.py instrument-class SHFQA
-python generator.py instrument-class SHFSG
-python generator.py instrument-class SHFQC
-python generator.py instrument-class HDAWG
-python generator.py instrument-class PQSC
-python generator.py instrument-class UHFLI
-python generator.py instrument-class UHFQA
+python generator/codegen/generator.py instrument-class SHFQA
+python generator/codegen/generator.py instrument-class SHFSG
+python generator/codegen/generator.py instrument-class SHFQC
+python generator/codegen/generator.py instrument-class HDAWG
+python generator/codegen/generator.py instrument-class PQSC
+python generator/codegen/generator.py instrument-class UHFLI
+python generator/codegen/generator.py instrument-class UHFQA
